69b1fc481be7777e31a2467ef1928b76973892a1
Mikhail Shulepov <mikhail.shulepov@gmail.com>
2022-01-12 16:04:50 +0300

    Filters

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import re
 app_id = 12983384
 api_hash = '641d1d4b6b72e34e296f128ad84e4c97'
 pattern = re.compile('(https?:\/\/.+?\.\w+)|(\w+\.(?:com|io|online|fi|org|ru|finance|fund|network|money|co|exchange|xyz|app|cash|ai))')
+filters = re.compile('youtube')
 
 #нарния чат -1001369370434
 #lobsterdao -1001242127973
@@ -20,8 +21,9 @@ async def my_event_handler(event):
     chat_id = event.chat_id
     if chat_id in chats:
         if pattern.search(event.raw_text) is not None:
-            print(event.raw_text)
-            await event.forward_to(forwardChat)
+            if filters.search(event.raw_text) is None:
+                print(event.raw_text)
+                await event.forward_to(forwardChat)
 
 client.start()
 client.run_until_disconnected()
