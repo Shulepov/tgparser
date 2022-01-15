64373b8411292ef25c9ece67e97674e1d5731d33
Mikhail Shulepov <mikhail.shulepov@gmail.com>
2022-01-15 13:02:47 +0300

    Fix

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import re
 app_id = 12983384
 api_hash = '641d1d4b6b72e34e296f128ad84e4c97'
 pattern = re.compile('(https?:\/\/.+?\.\w+)|(\w+\.(?:com|io|online|fi|org|ru|finance|fund|network|money|co|exchange|xyz|app|cash|ai))')
-filters = re.compile('youtu|coinmarketcap.com|etherscan.io|binance.com|')
+filters = re.compile('youtu|coinmarketcap.com|etherscan.io|binance.com')
 
 #нарния чат -1001369370434
 #lobsterdao -1001242127973
